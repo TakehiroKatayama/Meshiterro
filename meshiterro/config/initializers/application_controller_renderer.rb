0781e1d135ddb7ac7c1f5d94b1ad43b00a3cf1f9
TakehiroKatayama <t.katayama.info@gmail.com>
2021-04-09 07:49:45 +0000

    [finish]4-3

--- meshiterro/config/initializers/application_controller_renderer.rb
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '940b965d9baa78c14b0c7259604c53634f5e2ee07ed5dc203daf96506baddc4dad27c406fa74a189215a3b8ddcbb69cbce91936f7085bea3a6781dac8a47a4a4'
