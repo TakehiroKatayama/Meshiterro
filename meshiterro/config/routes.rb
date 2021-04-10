7aa47a52043b64aaf190701901573b1be8b711a3
TakehiroKatayama <t.katayama.info@gmail.com>
2021-04-10 06:23:05 +0000

    [fix]top_page

--- meshiterro/config/routes.rb
+++ meshiterro/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'users/show'
+  root'post_images#index'
   devise_for :users
-  root to: 'homes#top'
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
     resource :favorites, only: [:create, :destroy]
 
